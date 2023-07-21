67371086ff6215e3ae476f617c4215a01ed767af
hase <s2110327@coins.tsukuba.ac.jp>
2023-07-21 15:58:56 +0900

    交通情報を取得する関数を作成

--- otecom_flutter/lib/main.dart
+++ otecom_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
 
 void main() => runApp(MyApp());
 
@@ -31,6 +32,7 @@ class ViewWeatherImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    fetchTrainInfo("dummy");
     return Scaffold(
       appBar: AppBar(
 
@@ -42,6 +44,37 @@ class ViewWeatherImage extends StatelessWidget {
       ),
     );
   }
+
+  Future<List<List<String>>> fetchTrainInfo(String dummyLines) async {
+    final url = 'https://transit.yahoo.co.jp/diainfo/area/4';
+    final target = Uri.parse(url);
+    List<String> lines = ["つくばエクスプレス線"];
+    final response = await http.get(target);
+
+    if (response.statusCode != 200) {
+      print('ERROR: ${response.statusCode}');
+      return [];
+    }
+
+    final document = parse(response.body);
+    final result = document.querySelectorAll(
+        '.elmTblLstLine > table > tbody > tr > td').map((v) => v.text).toList();
+    List<List<String>> trainInfoList = [];
+    List<String> trainInfo = [];
+    for (int i = 0; i < result.length; i++) {
+      trainInfo.add(result[i]);
+      if (i % 3 == 2) {
+        if (lines.contains(trainInfo[0])) {
+          trainInfoList.add(trainInfo);
+        }
+        trainInfo = [];
+      }
+    }
+    print(trainInfoList);
+    return trainInfoList;
+  }
 }
+
+
 
 
